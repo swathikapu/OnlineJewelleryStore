871a92f4c604f67b3d1e966e47e0348a067d6536
Swathi Kapu <swathipentapalli@gmail.com>
2019-08-08 00:02:21 +0100

    Few metadata attrs added

---
+++ OnlineJewelleryStore/Models/ModelsMetadata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineJewelleryStore.Models
+{
+    [MetadataType(typeof(Tbl_ProductMetaData))]
+    public partial class Tbl_Product
+    {
+
+    }
+    public class Tbl_ProductMetaData
+    {
+        [DataType(DataType.Currency)]
+        public Nullable<decimal> Price { get; set; }
+    }
+   
+}
