b2cadd5cb6171d48d0d6f80552672b3a501c2101
Swathi Kapu <swathipentapalli@gmail.com>
2019-08-11 01:13:42 +0100

    First version of Ajax working

--- OnlineJewelleryStore/Controllers/CartController.cs
+++ OnlineJewelleryStore/Controllers/CartController.cs
@@ -11,18 +11,18 @@ namespace OnlineJewelleryStore.Controllers
     public class CartController : Controller
     {
         MainRepository mainRepo = new MainRepository();
-        public ActionResult Add(int productId, int quantity)
-        {
-            Tbl_Product product = mainRepo.GetProductById(productId);
-            CartItem item = new CartItem() { Product = product, Quantity = quantity };
-            Cart cart;
-            if (Session["cart"] == null)
-                cart = new Cart();
-            else
-                cart = (Cart)Session["cart"];
-            cart.Add(item);
-            Session["cart"] = cart;    // update cart with new details.
-            return RedirectToAction("Index", "Home");
-        }
+        //public ActionResult Add(int productId, int quantity)
+        //{
+        //    Tbl_Product product = mainRepo.GetProductById(productId);
+        //    CartItem item = new CartItem() { Product = product, Quantity = quantity };
+        //    Cart cart;
+        //    if (Session["cart"] == null)
+        //        cart = new Cart();
+        //    else
+        //        cart = (Cart)Session["cart"];
+        //    cart.Add(item);
+        //    Session["cart"] = cart;    // update cart with new details.
+        //    return RedirectToAction("Index", "Home");
+        //}
     }
 }
--- OnlineJewelleryStore/Controllers/HomeController.cs
+++ OnlineJewelleryStore/Controllers/HomeController.cs
@@ -18,15 +18,29 @@ namespace OnlineJewelleryStore.Controllers
             return View(mainRepo);
         }
 
+        public ActionResult Add(int productId, int quantity)
+        {
+            Tbl_Product product = mainRepo.GetProductById(productId);
+            CartItem item = new CartItem() { Product = product, Quantity = quantity };
+            Cart cart;
+            if (Session["cart"] == null)
+                cart = new Cart();
+            else
+                cart = (Cart)Session["cart"];
+            cart.Add(item);
+            Session["cart"] = cart;    // update cart with new details.
+            return PartialView("_Productpartial", product);
+        }
+
         public ActionResult Products(int categoryId)
         {
             List<Tbl_Product> products = mainRepo.GetProductsByCategoryId(categoryId);
             return View(products);
         }
 
-        public ActionResult _Productpartial(int id)
+        public ActionResult _Productpartial(int ?id)
         {
-            Tbl_Product product = mainRepo.GetProductById(id);
+            Tbl_Product product = mainRepo.GetProductById((int)id);
             return PartialView(product);
         }
 
